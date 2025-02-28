a5832c8b4375fb48978f893860edbca2602066ab
mukuvi <jamesmngandu@gmail.com>
2025-03-01 02:15:02 +0300

    motion

--- motion/src/components/Result.jsx
+++ motion/src/components/Result.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Result = ({ data }) => {
+  const { homeValue, downPayment, loanAmount, loanTerm, interestRate } = data;
+  const totalLoanMonth = loanTerm * 12; //60 months = 5years
+  const interestPerMonth = interestRate / 100 / 12;
+  const monthlyPayment =
+    (loanAmount * interestPerMonth * (1 + interestPerMonth) ** totalLoanMonth) /
+    ((1 + interestPerMonth) ** totalLoanMonth - 1);
+  const totalInterestGenerated = monthlyPayment * totalLoanMonth - loanAmount;
   return <div>Result</div>;
 };
 
