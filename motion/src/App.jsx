29052f2ed18f373dfb07f3e346c371f85e4b4f1d
mukuvi <jamesmngandu@gmail.com>
2025-03-01 00:27:17 +0300

    motion

--- motion/src/App.jsx
+++ motion/src/App.jsx
@@ -7,9 +7,16 @@ import TenureSelect from "./components/TenureSelect";
 import { Container, Grid } from "@mui/material";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 300,
+    downPayment: 3000,
+    loanAmount: 3000,
+    loanAmount: 100,
+  });
   return (
     <div className="App">
       <Navbar />
+
       <Container maxWidth="xl" sx={{ mt: 4 }}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={6}>
