f711163846b674672e1a6d036837af7a5babd1b5
mukuvi <jamesmngandu@gmail.com>
2025-02-28 22:17:24 +0300

    motion

--- motion/src/components/Navbar.jsx
+++ motion/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <AppBar position="static">
+        <Container maxWidth="sm">
+          <Toolbar>
+            <Typography variant="h4" component="p">
+              Motion
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Navbar;
