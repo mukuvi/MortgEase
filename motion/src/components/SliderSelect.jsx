a25f490536cbd0a699c870db6fce3afeb9ce1b01
mukuvi <jamesmngandu@gmail.com>
2025-03-01 01:16:08 +0300

    motion

--- motion/src/components/SliderSelect.jsx
+++ motion/src/components/SliderSelect.jsx
@@ -14,20 +14,31 @@ const SliderSelect = ({ data, setData }) => {
         defaultValue={data.homeValue}
         value={data.homeValue}
         step={100}
-        onChange={(e, value) => setData({ homeValue: value })}
+        onChange={(e, value) => setData({ ...data, homeValue: value })}
       />
       <SliderComponent
         label="Down Payment"
         unit="$"
-        amount={1000}
-        defaultValue={80}
-        min={5}
-        max={100}
-        step={10}
-        onChange={(e, value) => {}}
+        amount={data.downPayment}
+        defaultValue={data.downPayment}
+        value={data.downPayment}
+        min={1500}
+        max={10000}
+        step={100}
+        onChange={(e, value) => setData({ ...data, downPayment: value })}
       />
       <SliderComponent
         label="Loan Value"
+        unit="$"
+        amount={2000}
+        defaultValue={80}
+        min={50}
+        max={100}
+        step={10}
+        onChange={(e, value) => {}}
+      />{" "}
+      <SliderComponent
+        label="Interest Rate"
         unit="$"
         amount={2000}
         defaultValue={80}
